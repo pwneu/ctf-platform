97474527da3c26b7ade5d19c773defadb75a0d3e
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-06 03:56:33 +0800

    Set links for the footer section

---
+++ src/data/footerLinks.js
@@ -0,0 +1,39 @@
+export const footerLinks = [ 
+  {
+    title: "ABOUT US",
+    links: [
+      { href: "/our-story", label: "About Us" },
+      { href: "/who-we-are", label: "Who We are" },
+      { href: "/contact", label: "Contact Us" },
+    ],
+  },
+  {
+    title: "Resources",
+    links: [
+      { href: "/achievements-list", label: "Articles" },
+      { href: "/career-development", label: "Road Map" },
+      { href: "", label: "Recommended Tools" },
+      { href: "/resource-libraries", label: "Resource Libraries" },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      { href: "", label: "Learners Stories" },
+      { href: "", label: "Campuses" },
+      { href: "/discussion-forum", label: "Discord" },
+    ],
+  },
+
+  {
+    title: "SUPPORT",
+    links: [
+      
+      { href: "/help-center", label: "Help Center" },
+      { href: "", label: "Privacy Policy" },
+      { href: "", label: "FAQs" },
+      { href: "", label: "Terms and Conditions" },
+
+    ],
+  },
+];
