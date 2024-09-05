20055bc509464019b145dbc6b4e9729fc410e5ec
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-06 03:56:57 +0800

    Add Outline learning paths

---
+++ src/data/learningPaths.js
@@ -0,0 +1,60 @@
+export const learningPath = [ 
+  {
+    id: "item1",
+    iconBg: "bg-green-2",
+    iconSrc: "/assets/img/learningPaths/learning/icons/1.svg",
+    title: "Cybersecurity Training",
+    description:
+      "Enhance your skills with our in-depth courses covering a  wide range of cybersecurity topics, \n from fundamental principles to advanced techniques.",
+  },
+  {
+    id: "item2",
+    iconBg: "bg-purple-2",
+    iconSrc: "/assets/img/learningPaths/learning/icons/2.svg",
+    title: "Hands-On Labs and Simulations",
+    description:
+      "Gain practical experience through interactive labs and simulations that replicate real-world scenarios, helping you to apply what you've learned in a safe environment.",
+  },
+  {
+    id: "item3",
+    iconBg: "bg-blue-",
+    iconSrc: "/assets/img/learningPaths/learning/icons/3.svg",
+    title: "Expert-Led Instruction",
+    description:
+      "Learn from industry professionals with extensive experience in cybersecurity, offering insights and guidance to help you stay ahead in the field.",
+  },
+  {
+    id: "item3",
+    iconBg: "bg-orange-2",
+    iconSrc: "/assets/img/learningPaths/learning/icons/3.svg",
+    title: "Up-to-Date Content",
+    description:
+      "Stay current with the latest trends and threats in cybersecurity through continuously  updated content that reflects the evolving landscape of digital security.",
+  },
+  {
+    id: "item3",
+    iconBg: "bg-orange-2",
+    iconSrc: "/assets/img/learningPaths/learning/icons/3.svg",
+    title: "Flexible Learning Options",
+    description:
+      "Access courses at your own pace, anytime and anywhere, with our user-friendly LMS platform designed to fit into your busy schedule.",
+  },
+  {
+    id: "item3",
+    iconBg: "bg-orange-2",
+    iconSrc: "/assets/img/learningPaths/learning/icons/3.svg",
+    title: "Engaging Capture the Flag (CTF) Competitions",
+    description:
+      "Test your skills in real-world scenarios with our exciting CTF challenges. Compete against peers, solve complex problems, and earn recognition for your achievements.",
+  },
+  {
+    id: "item3",
+    iconBg: "bg-orange-2",
+    iconSrc: "/assets/img/learningPaths/learning/icons/3.svg",
+    title: "Interactive Webinars and Events",
+    description:
+      "Participate in our regular webinars and events hosted by cybersecurity experts. Stay informed about industry developments, gain practical insights, and engage with the cybersecurity community.",
+  },
+];
+
+
