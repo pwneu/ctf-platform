46a23f2b4f8314e451a9ffadf822cc6bc3a95a4b
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-06 04:17:57 +0800

    Display main footer section

---
+++ src/components/layout/footers/Footer.jsx
@@ -0,0 +1,61 @@
+import React from "react"; 
+import { Link } from "react-router-dom"; 
+import Socials from "@/components/common/Socials"; 
+import FooterLinks from "../component/FooterLinks"; 
+import Links from "../component/Links"; 
+export default function Footer() {
+ 
+  return (
+    <footer className="footer -type-1 bg-dark-1 -green-links">
+      <div className="container">
+        <div className="footer-header">
+          <div className="row y-gap-20 justify-between items-center">
+            <div className="col-auto">
+              <div className="footer-header__logo">
+                <img src="/assets/img/general/PWNEU_Logo.svg" alt="logo" />
+              </div>
+            </div>
+            <div className="col-auto">
+              <div className="footer-header-socials">
+                <div className="footer-header-socials__title text-white">
+                  Follow us on social media
+                </div>
+                <div className="footer-header-socials__list">
+                  <Socials />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="footer-columns">
+          <div className="row y-gap-30">
+            <FooterLinks
+              allClasses={"text-17 fw-500 text-white uppercase mb-25"}
+            />
+          </div>
+        </div>
+
+        <div className="py-30 border-top-light-15">
+          <div className="row justify-between items-center y-gap-20">
+            <div className="col-auto">
+              <div className="d-flex items-center h-100 text-white">
+                © {new Date().getFullYear()} PWNEU. All Right Reserved.
+              </div>
+            </div>
+
+            <div className="col-auto">
+              <div className="d-flex x-gap-20 y-gap-20 items-center flex-wrap">
+                <div>
+                  <div className="d-flex x-gap-15 text-white">
+                    <Links />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
