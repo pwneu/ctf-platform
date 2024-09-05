a4794a1bda6bb8a5c47c7b50777379e64a88b05f
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-06 03:59:14 +0800

    Add user testimonials

---
+++ src/data/tesimonials.js
@@ -0,0 +1,22 @@
+export const testimonials = [
+  {
+    id: 0,
+    imageSrc: "/assets/img/testimonials/1.png",
+    name: "David King Roderos",
+    position: "BS in Computer Science",
+    comment: "Outstanding Experience",
+    description: `Participating in the University CTF 2023 was an incredible experience. The challenges were well-crafted, pushing us to think critically and apply our skills in real-time scenarios. The event was organized flawlessly, with excellent support and resources provided throughout. It truly showcased the university's commitment to fostering a strong cybersecurity community`,
+  },
+  {
+    id: 1,
+    imageSrc: "/assets/img/testimonials/1.png",
+    name: "Sharon Grace Hangaan",
+    position: "BS in Computer Science",
+    comment: "Great Work",
+    description: `Participating in the University CTF 2023 was an incredible experience. The challenges were well-crafted, pushing us to think critically and apply our skills in real-time scenarios. The event was organized flawlessly, with excellent support and resources provided throughout. It truly showcased the university's commitment to fostering a strong cybersecurity community`,
+  },
+];
+
+
+
+
