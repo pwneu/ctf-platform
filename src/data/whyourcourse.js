660b7891d7d7e86ff32a43042dc6c9c9b8769efe
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-06 04:00:07 +0800

    Add reasons and benefits of the platform

---
+++ src/data/whyourcourse.js
@@ -0,0 +1,21 @@
+export const whyourcourse = [
+  {
+    id: 1,
+    icon: "icon-online-learning-4 text-64 text-green-1",
+    title: "01. Level Up Your Skills",
+    text: "Embark on a cybersecurity adventure with hands-on challenges and real-world scenarios. Our courses are like a Capture the Flag (CTF) game, designed to enhance your hacking and defense skills while keeping you engaged",
+  },
+  {
+    id: 2,
+    icon: "icon-graduation-1 text-64 text-green-1",
+    title: "02. Master Cybersecurity Techniques",
+    text: "Conquer complex security puzzles and gain practical expertise. Our training is crafted to equip you with the skills and knowledge needed to excel in the dynamic field of cybersecurity.",
+  },
+  {
+    id: 3,
+    icon: "icon-working-at-home-2 text-64 text-green-1",
+    title: "03. Enter the Cyber Arena",
+    text: "Prepare to tackle real-world security challenges. With our training, you’ll be ready to dive into the cybersecurity landscape and make a difference in protecting digital assets.",
+  },
+];
+
