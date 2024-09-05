67bcf354d45d7239a26ddeec7afb3da3895c965c
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-06 03:54:51 +0800

    Provide campus location details

---
+++ src/data/campuses.js
@@ -0,0 +1,25 @@
+export const campuses = [ 
+    {
+      id: 1,
+      imgSrc: "/assets/img/campuses/1.png",
+      title: "Main Campus",
+      desc: "The New Era Educational Institute (NEEI) was formally opened on June 17, 1975",   
+    },
+    {
+      id: 2,
+      imgSrc: "/assets/img/campuses/1.png",
+      title: "Batangas Branch",
+      desc: "As part of the celebration of New Era University's 25th anniversary, the New Era University – Lipa City Branch was established on May 5, 2000 in Batangas.",   
+    },
+    {
+      id: 3,
+      imgSrc: "/assets/img/campuses/1.png",
+      title: "Pampanga Branch",
+      desc: "New Era University's first branch outside Metro Manila is situated in a sprawling six-hectare campus in the City of San Fernando, Pampanga.",   
+    },
+  ];
+  
+
+  
+
+  
