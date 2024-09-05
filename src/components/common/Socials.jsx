43b3a3639164ea474372984b487fb2e4c19de02c
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-06 04:09:45 +0800

    Create social media links

---
+++ src/components/common/Socials.jsx
@@ -0,0 +1,18 @@
+import { socialMediaLinks } from "../../data/socialLinks"; 
+import React from "react";
+
+export default function Socials({ componentsClass, textSize }) {
+  return (
+    <>
+      {socialMediaLinks.map((link, index) => (
+        <a
+          key={index}
+          className={componentsClass ? componentsClass : ""}
+          href={link.href}
+        >
+          <i className={`${link.iconClassName} ${textSize}`}></i>
+        </a>
+      ))}
+    </>
+  );
+}
