0a916c75d38a639b96fe0832c41b2c851f4ebeee
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-06 04:24:21 +0800

    Present team information

---
+++ src/pages/about/who-we-are/index.jsx
@@ -0,0 +1,29 @@
+import WhoWeAre from "@/components/about/WhoWeAre"; 
+import PageLinks from "@/components/common/PageLinks"; 
+import Preloader from "@/components/common/Preloader"; 
+import Footer from "@/components/layout/footers/Footer"; 
+import Header from "@/components/layout/headers/Header"; 
+import React from "react"; 
+
+import MetaComponent from "@/components/common/MetaComponent"; 
+
+const metadata = {
+  title:
+    "Who We Are || PWNEU",
+    description: "Get to know PWNEU—our team, our values, and what makes us unique in the world of competitive challenges.",
+};
+
+export default function WhoWeAres() {
+  return (
+    <div className="main-content  ">
+      <MetaComponent meta={metadata} />
+      <Preloader />
+      <Header />
+      <div className="content-wrapper js-content-wrapper overflow-hidden">
+        <PageLinks />
+        <WhoWeAre />
+        <Footer />
+      </div>
+    </div>
+  );
+}
