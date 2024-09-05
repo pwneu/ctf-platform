c2ff30ed01cd0c2b29fa4ef67504019692cd9507
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-06 03:57:54 +0800

    Modify general link data for navigation

---
+++ src/data/links.js
@@ -0,0 +1,7 @@
+export const links = [ 
+  { id: 1, href: "/help-center", label: "Help" },
+  { id: 2, href: "", label: "Privacy Policy" },
+  { id: 3, href: "", label: "Cookie Notice" },
+  { id: 4, href: "", label: "Security" },
+  { id: 5, href: "", label: "Terms of Use" },
+];
