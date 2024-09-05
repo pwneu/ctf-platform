66da23bcee7c092648c95e58fb69916e0a3dcc00
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-06 04:24:09 +0800

    Show Organization’s story

---
+++ src/pages/about/our-story/index.jsx
@@ -0,0 +1,29 @@
+import OurStory from "@/components/about/OurStory"; 
+import PageLinks from "@/components/common/PageLinks"; 
+import Preloader from "@/components/common/Preloader"; 
+import Footer from "@/components/layout/footers/Footer"; 
+import Header from "@/components/layout/headers/Header"; 
+import React from "react"; 
+
+import MetaComponent from "@/components/common/MetaComponent"; 
+
+const metadata = {
+  title:"Our Story || PWNEU",
+  description: "Learn about our journey, from humble beginnings to where we are today, and the values that drive us.",
+};
+
+export default function OurStorys() {
+  return (
+    <div className="main-content  ">
+      <MetaComponent meta={metadata} />
+      <Preloader />
+
+      <Header />
+      <div className="content-wrapper js-content-wrapper overflow-hidden">
+        <PageLinks />
+        <OurStory />
+        <Footer />
+      </div>
+    </div>
+  );
+}
