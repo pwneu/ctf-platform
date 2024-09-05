686c441e84b4e45e9a4204514c6e1ac4c7b86ed9
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-06 03:56:05 +0800

    List contact information links

---
+++ src/data/contactLinks.js
@@ -0,0 +1,17 @@
+export const contactData = [
+  {
+    id: 1,
+    icon: "/assets/img/contact/1.svg",
+    address: "9 Central Ave, Quezon City, 1107 Metro Manila",
+  },
+  {
+    id: 2,
+    icon: "/assets/img/contact/2.svg",
+    phoneNumber: "+ (02) 8981 4221",
+  },
+  {
+    id: 3,
+    icon: "/assets/img/contact/3.svg",
+    email: "http://www.neu.edu.ph/",
+  },
+];
