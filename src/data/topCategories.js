49ad22502fcb7def4ac42bf9fdb9d2b1e3a76262
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-06 03:59:32 +0800

    Define top categories

---
+++ src/data/topCategories.js
@@ -0,0 +1,45 @@
+export const topCatagoriesThree = [
+  {
+    id: 1,
+    imageSrc: "/assets/img/categories/1.png",
+    title: "Digital Marketing",
+    courses: "573+ Courses",
+  },
+  {
+    id: 2,
+    imageSrc: "/assets/img/categories/1.png",
+    title: "Web Development",
+    courses: "473+ Courses",
+  },
+  {
+    id: 3,
+    imageSrc: "/assets/img/categories/1.png",
+    title: "Graphic Design",
+    courses: "583+ Courses",
+  },
+  {
+    id: 4,
+    imageSrc: "/assets/img/categories/1.png",
+    title: "Social Sciences",
+    courses: "373+ Courses",
+  },
+  {
+    id: 5,
+    imageSrc: "/assets/img/categories/2.png",
+    title: "Personal Development",
+    courses: "373+ Courses",
+  },
+  {
+    id: 6,
+    imageSrc: "/assets/img/categories/1.png",
+    title: "Photography",
+    courses: "393+ Courses",
+  },
+  {
+    id: 7,
+    imageSrc: "/assets/img/categories/1.png",
+    title: "Arts and Humanities",
+    courses: "373+ Courses",
+  },
+];
+
