eba12e827ef194b16aea335fe9d5d3ef66a53bc2
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-06 03:58:08 +0800

    Add Define menu items

---
+++ src/data/menu.js
@@ -0,0 +1,47 @@
+
+export const menuList = [
+  {
+    title: "Learn",
+    links: [
+      {
+        href: "#" 
+      }
+     
+    ],
+  },
+  {
+    title: "Compete",
+    links: [
+      {
+        href: "/list-of-challenges" 
+      }
+     ],
+  },
+  {
+    title: "Campuses",
+    links: [
+      {
+        href: "#" 
+      }
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      { href: "/achievements-list", label: "Achievement Highlights" },
+      { href: "/discussion-forum", label: "Discussion Forums" },
+      { href: "/learning-paths", label: "Learning Paths" },
+      { href: "/resource-libraries", label: "Resource Libraries" },
+      { href: "/career-development", label: "Career Development" },
+     
+    ],
+  },
+  {
+    title: "About Us",
+    links: [
+      { href: "/our-story", label: "Our Story" },
+      { href: "/who-we-are", label: "Who We Are" },
+      { href: "/mission-vision", label: "Mission and Vision" },
+    ],
+  },
+];
