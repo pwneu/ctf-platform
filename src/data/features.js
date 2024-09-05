a6dd969f751a95a5c77bda8a36359145a6441fce
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-06 03:56:19 +0800

    Define application features

---
+++ src/data/features.js
@@ -0,0 +1,93 @@
+export const feature = [
+  {
+    id: 1,
+    title: "High-Quality Learning: Engage with thoughtfully designed courses.",
+  },
+  {
+    id: 2,
+    title: "Student-Friendly: Enjoy a clear, easy-to-follow curriculum.",
+  },
+  {
+    id: 3,
+    title: "Free Access: Gain valuable knowledge at no cost.",
+  },
+  {
+    id: 4,
+    title: "Open and Inclusive: Join the platform and start learning right away without any fees.",
+  },
+];
+
+
+
+export const teachingFeatures = [
+  {
+    id: 1,
+    title: "Last Education of Bachelor Degree",
+  },
+  {
+    id: 2,
+    title: "More Than 15 Years Experience",
+  },
+  {
+    id: 3,
+    title: "12 Education Award Winning",
+  },
+];
+
+export const statictis = [
+  {
+    id: 1,
+    rating: "9/10",
+    description: "9/10 users reported better learning outcomes.",
+  },
+  {
+    id: 2,
+    rating: "85%",
+    description: "85% of students see their course through to completion",
+  },
+];
+
+export const featuresEight = [
+  {
+    id: 1,
+    icon: "/assets/img/home-8/what/icons/1.svg",
+    title: "Industry expert teachers",
+    text: "Lorem ipsum dolor sit amet, consectetur dolorili adipiscing elit. Felis donec massa aliquam id dolor .",
+    delay: 2,
+  },
+  {
+    id: 2,
+    icon: "/assets/img/home-8/what/icons/2.svg",
+    title: "Up-to-date course content",
+    text: "Lorem ipsum dolor sit amet, consectetur dolorili adipiscing elit. Felis donec massa aliquam id dolor .",
+    delay: 3,
+  },
+  {
+    id: 3,
+    icon: "/assets/img/home-8/what/icons/3.svg",
+    title: "Students community",
+    text: "Lorem ipsum dolor sit amet, consectetur dolorili adipiscing elit. Felis donec massa aliquam id dolor .",
+    delay: 4,
+  },
+];
+
+export const statictisEight = [
+  {
+    id: 1,
+    score: "9/10",
+    text: "Overall courses satisfaction score",
+    className: "-el-1",
+  },
+  {
+    id: 2,
+    score: "10K+",
+    text: "Happy Students Worldwide",
+    className: "-el-2",
+  },
+  {
+    id: 3,
+    score: "96%",
+    text: "Completition Rate On All Courses",
+    className: "-el-3",
+  },
+];
