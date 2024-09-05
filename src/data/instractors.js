d6ed09cf8ac763b4c07cce5db7d010241c309325
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-06 03:56:42 +0800

    Provide instructor profiles and details

---
+++ src/data/instractors.js
@@ -0,0 +1,18 @@
+export const instractors = [
+  {
+    id: 1,
+    image: "/assets/img/orgmentors/1.png",
+    name: "New Era University",
+  },
+  {
+    id: 2,
+    image: "/assets/img/orgmentors/1.png",
+    name: "College of Informatics and Computing Studies",
+  },
+  {
+    id: 3,
+    image: "/assets/img/orgmentors/1.png",
+    name: "College of Criminology",
+  },
+  
+];
