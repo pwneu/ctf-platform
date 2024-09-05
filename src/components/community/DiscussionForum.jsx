3175b42aac0366286f893a73d9fc81cc5e319cec
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-06 04:11:10 +0800

    Manage discussion forum

---
+++ src/components/community/DiscussionForum.jsx
@@ -0,0 +1,36 @@
+import React from "react"; 
+
+import { Link } from "react-router-dom";
+export default function DiscussionForum() {
+  return (
+    <>
+      <section className="page-header -type-1">
+        <div className="container">
+          <div className="page-header__content">
+            <div className="row justify-center text-center">
+              <div className="col-auto">
+                <div>
+                  <h1 className="page-header__title">Discussion Forum</h1>
+                </div>
+
+                <div>
+                  <p className="page-header__text">
+                  Neque convallis a cras semper auctor. Libero id faucibus nisl
+                  tincidunt egetnvallis a cras se
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="layout-pt-md layout-pb-lg">
+        <div className="container">
+          <div className="row y-gap-50 justify-between items-center">
+    
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
