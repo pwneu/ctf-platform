795232351e12ff6a32820061cd4faa0fd30e1f05
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-06 04:24:49 +0800

    Display list of challenges

---
+++ src/pages/challengesList/list-of-challenges/index.jsx
@@ -0,0 +1,28 @@
+import PageLinks from "@/components/common/PageLinks"; 
+import Preloader from "@/components/common/Preloader"; 
+import ChallengesList from "@/components/challengesList/ChallengesList"; 
+import Footer from "@/components/layout/footers/Footer"; 
+import Header from "@/components/layout/headers/Header"; 
+import React from "react"; 
+
+import MetaComponent from "@/components/common/MetaComponent"; 
+
+const metadata = {
+  title: "Compete || PWNEU",
+  description: "Join the competition, test your skills, and climb the leaderboard in exciting challenges.",
+};
+
+export default function CourseListPage1() {
+  return (
+    <div className="main-content  ">
+      <MetaComponent meta={metadata} />
+      <Preloader />
+      <Header />
+      <div className="content-wrapper  js-content-wrapper overflow-hidden">
+        <PageLinks />
+        <ChallengesList  />
+        <Footer />
+      </div>
+    </div>
+  );
+}
