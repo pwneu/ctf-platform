bc14105a47f161db34ea33053542af47b359fd78
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-06 04:09:05 +0800

    Manage navigation links

---
+++ src/components/common/PageLinks.jsx
@@ -0,0 +1,10 @@
+
+import { Link } from "react-router-dom";
+import React from "react";
+
+export default function PageLinks({ dark }) {
+  return (
+    <section className={`breadcrumbs ${dark ? "bg-dark-2" : ""} `}>
+    </section>
+  );
+}
