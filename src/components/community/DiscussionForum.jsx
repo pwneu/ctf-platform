3205f9c96b7357fa7d734b32beb20e9d83457986
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-13 23:34:17 +0800

    Add Community btn

--- src/components/community/DiscussionForum.jsx
+++ src/components/community/DiscussionForum.jsx
@@ -4,30 +4,36 @@ import { Link } from "react-router-dom";
 export default function DiscussionForum() {
   return (
     <>
-      <section className="page-header -type-1">
-        <div className="container">
-          <div className="page-header__content">
-            <div className="row justify-center text-center">
-              <div className="col-auto">
-                <div>
-                  <h1 className="page-header__title">Discussion Forum</h1>
-                </div>
-
-                <div>
-                  <p className="page-header__text">
-                  Neque convallis a cras semper auctor. Libero id faucibus nisl
-                  tincidunt egetnvallis a cras se
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
       <section className="layout-pt-md layout-pb-lg">
         <div className="container">
-          <div className="row y-gap-50 justify-between items-center">
-    
+          <div className="row y-gap-60 justify-between items-center">
+            <div className="col-lg-5">
+              <h4 className="text-45 lh-16">
+                  Join Our Community
+              </h4>
+              <p className="text-dark-1 mt-30 text-19">
+                Connect with like-minded individuals in our all-in-one community! Stay updated with learner stories, upcoming events, 
+                and join our active Discord and forums. PWNEU is your go-to place for everything you need in the world of cybersecurity education.
+              </p>
+            
+              <div className="d-inline-block">
+                <Link
+                  to="/signup"
+                  className="button -md -purple-1 text-white mt-50"
+                >
+                  Join Now
+                </Link>
+              </div>
+            </div>
+            <div className="col-lg-6 pr-50 sm:pr-15">
+              <div className="composition -type-8">
+               <img
+                    className="w-1/1"
+                    src="/assets/img/community/Community_Discord.svg"
+                    alt="image"
+                  />
+              </div>
+            </div>
           </div>
         </div>
       </section>
