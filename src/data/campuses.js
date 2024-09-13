45f980a7eb835d72a47768ce428b3f0f118ea29b
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-13 23:27:28 +0800

    Add Image Source

--- src/data/campuses.js
+++ src/data/campuses.js
@@ -1,21 +1,33 @@
 export const campuses = [ 
     {
       id: 1,
-      imgSrc: "/assets/img/campuses/1.png",
+      imgSrc: "/assets/img/campuses/MainBranch_Card.svg",
       title: "Main Campus",
-      desc: "The New Era Educational Institute (NEEI) was formally opened on June 17, 1975",   
+      desc: "The New Era Educational Institute (NEEI) was formally opened on June 17, 1975 by virtue of GPR No. 232, Series of 1975 issued by the Ministry of Education, Culture and Sports.",   
     },
     {
       id: 2,
-      imgSrc: "/assets/img/campuses/1.png",
-      title: "Batangas Branch",
+      imgSrc: "/assets/img/campuses/BatangasBranch_Card.svg",
+      title: "Batangas",
       desc: "As part of the celebration of New Era University's 25th anniversary, the New Era University – Lipa City Branch was established on May 5, 2000 in Batangas.",   
     },
     {
       id: 3,
-      imgSrc: "/assets/img/campuses/1.png",
-      title: "Pampanga Branch",
+      imgSrc: "/assets/img/campuses/PampangaBranch_Card.svg",
+      title: "Pampanga",
       desc: "New Era University's first branch outside Metro Manila is situated in a sprawling six-hectare campus in the City of San Fernando, Pampanga.",   
+    },
+    {
+      id: 4,
+      imgSrc: "/assets/img/campuses/GeneralSantosBranch_Card.svg",
+      title: "General Santos",
+      desc: "New Era University's first branch outside Metro Manila is situated in a sprawling six-hectare campus in the City of San Fernando, Pampanga.",   
+    },
+    {
+      id: 5,
+      imgSrc: "/assets/img/campuses/RizalBranch_Card.svg",
+      title: "Rizal",
+      desc: "The New Era University – Rizal Branch is the fifth and newest branch of the University. ",   
     },
   ];
   
