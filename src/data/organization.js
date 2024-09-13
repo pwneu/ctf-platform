955279653dd623e406c06da5347b3e652b62bff2
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-13 23:28:19 +0800

    Update Organization assets.svg

--- src/data/organization.js
+++ src/data/organization.js
@@ -1,8 +1,7 @@
 export const organization = [  
-  "/assets/img/clients/1.svg",
-  "/assets/img/clients/2.svg",
-  "/assets/img/clients/3.svg",
-  "/assets/img/clients/4.svg",
-  "/assets/img/clients/5.svg",
-  "/assets/img/clients/6.svg",
+  "/assets/img/organization/GuideM__Org.svg",
+  "/assets/img/organization/Secuna_Org.svg",
+  "/assets/img/organization/NEU__Org.svg",
+  "/assets/img/organization/NEUCICS_Org.svg",
+  
 ];
