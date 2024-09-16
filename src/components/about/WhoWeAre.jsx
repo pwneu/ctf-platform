8ee4e0ca29721585a266805669b4b8f224bad111
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-17 01:15:55 +0800

    Update About Component

--- src/components/about/WhoWeAre.jsx
+++ src/components/about/WhoWeAre.jsx
@@ -1,36 +1,40 @@
-import React from "react"; 
-
+import React from "react";
 import { Link } from "react-router-dom";
 export default function WhoWeAre() {
   return (
     <>
       <section className="page-header -type-1">
         <div className="container">
-          <div className="page-header__content">
-            <div className="row justify-center text-center">
-              <div className="col-auto">
-                <div>
-                  <h1 className="page-header__title">Who We Are</h1>
-                </div>
+          <div className="row y-gap-30 justify-between items-center">
+            <div className="col-xl-5 col-lg-6 col-md-9 lg:order-2">
+              <h3 className="text-40 lh-1 text-center">Who We Are</h3>
+              <p className="mt-25 text-center">
+                We are the CTF (Capture The Flag) Player, dedicated to providing
+                a dynamic and engaging learning platform for students pursuing
+                cybersecurity at New Era University. Our mission is to encourage
+                and empower students by offering them the tools, resources, and
+                support they need to excel in the field of cybersecurity. We
+                believe in hands-on learning through real-world challenges.
+                That's why we focus on creating an environment where students
+                can participate in CTF competitions, gain practical experience,
+                and develop critical problem-solving skills. Whether you're a
+                beginner just starting out or an advanced learner looking to
+                hone your skills, our platform offers a range of challenges that
+                cater to all skill levels.
+              </p>
+            </div>
 
-                <div>
-                  <p className="page-header__text">
-                  Neque convallis a cras semper auctor. Libero id faucibus nisl
-                  tincidunt egetnvallis a cras se
-                  </p>
-                </div>
-              </div>
+            <div className="col-lg-6 lg:order-1">
+              <img
+                className="w-1/1"
+                src="/assets/img/about/whoweare/whoweare_Card.svg"
+                alt="image"
+              />
             </div>
           </div>
         </div>
       </section>
-      <section className="layout-pt-md layout-pb-lg">
-        <div className="container">
-          <div className="row y-gap-50 justify-between items-center">
-    
-          </div>
-        </div>
-      </section>
+      v
     </>
   );
 }
