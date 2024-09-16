a9ebbf172878a3e19b08ac5d86fe84d4ed797196
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-17 01:23:35 +0800

    Update href Ownership

--- src/data/links.js
+++ src/data/links.js
@@ -1,7 +1,7 @@
 export const links = [ 
   { id: 1, href: "/help-center", label: "Help" },
-  { id: 2, href: "", label: "Privacy Policy" },
+  { id: 2, href: "/privacy-policy", label: "Privacy Policy" },
   { id: 3, href: "", label: "Cookie Notice" },
   { id: 4, href: "", label: "Security" },
-  { id: 5, href: "", label: "Terms of Use" },
+  { id: 5, href: "/terms-and-conditions", label: "Terms and Conditions" },
 ];
