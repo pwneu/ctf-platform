8c7419b7c79dfdf4f5ea6d034e0dfa08858c0c80
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-17 01:21:21 +0800

    Modify Imports

--- src/pages/about/our-story/index.jsx
+++ src/pages/about/our-story/index.jsx
@@ -1,15 +1,17 @@
-import OurStory from "@/components/about/OurStory"; 
-import PageLinks from "@/components/common/PageLinks"; 
-import Preloader from "@/components/common/Preloader"; 
-import Footer from "@/components/layout/footers/Footer"; 
-import Header from "@/components/layout/headers/Header"; 
-import React from "react"; 
+import OurStory from "@/components/about/OurStory";
+import PageLinks from "@/components/common/PageLinks";
+import Preloader from "@/components/common/Preloader";
+import Footer from "@/components/layout/footers/Footer";
+import Header from "@/components/layout/headers/Header";
+import React from "react";
+import OurApproach from "@/components/about/OurApproach";
+import HeroStory from "@/components/about/HeroStory";
 
-import MetaComponent from "@/components/common/MetaComponent"; 
-
+import MetaComponent from "@/components/common/MetaComponent";
 const metadata = {
-  title:"Our Story || PWNEU",
-  description: "Learn about our journey, from humble beginnings to where we are today, and the values that drive us.",
+  title: "Our Story || PWNEU",
+  description:
+    "Learn about our journey, from humble beginnings to where we are today, and the values that drive us.",
 };
 
 export default function OurStorys() {
@@ -21,7 +23,9 @@ export default function OurStorys() {
       <Header />
       <div className="content-wrapper js-content-wrapper overflow-hidden">
         <PageLinks />
+        <HeroStory />
         <OurStory />
+        <OurApproach />
         <Footer />
       </div>
     </div>
