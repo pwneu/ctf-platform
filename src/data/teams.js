d3f7a21015e7b19076c633115926dd6616ba2bd2
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-17 01:17:32 +0800

    Create DevTeams

---
+++ src/data/teams.js
@@ -0,0 +1,43 @@
+
+export const developers = [
+     {
+      id: 1,
+      title: "Aldwin Tapican",
+      profession: "Cyber Threat Analyst",
+    },
+    {
+      id: 2,
+      title: "Marjolo Mabuti",
+      profession: "Security Operations Center Analyst",
+    },
+    {
+      id: 3,
+      title: "Mark Owen Ramos",
+      profession: "Security Operations Center Analyst ",
+    },
+    {
+      id: 4,
+      title: "Andrew Felix Cunanan",
+      profession: "Information Security Analyst",
+    },
+    {
+      id: 5,
+      title: "Janel James Janiola",
+      profession: "Security Operations Center Analyst",
+    },
+    {
+      id: 6,
+      title: "David King  Roderos",
+      profession: "Software Engineer",
+    },
+    {
+      id: 7,
+      title: "Sharon Grace Hangaan",
+      profession: "Full-Stack Developer",
+    },
+    {
+      id: 8,
+      title: "Vina Elyssa De Guzman",
+      profession: "Cybersecurity Analyst",
+    },
+  ];
