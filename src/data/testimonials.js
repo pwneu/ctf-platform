8349a6b7ef1ea49d65654b9e60302d1c4749786e
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-17 01:22:21 +0800

    Create Testimonials

---
+++ src/data/testimonials.js
@@ -0,0 +1,71 @@
+export const testimonials = [
+    {
+      id: 1,
+      imageSrc: "/assets/img/testimonials/1.png",
+      name: "David",
+      position: "BS in Computer Science",
+      comment: "Outstanding Experience",
+      description: `Participating in the University CTF 2023 was an incredible experience. The challenges were well-crafted, pushing us to think critically and apply our skills in real-time scenarios. The event was organized flawlessly, with excellent support and resources provided throughout. It truly showcased the university's commitment to fostering a strong cybersecurity community`,
+    },
+    {
+      id: 2,
+      imageSrc: "/assets/img/testimonials/1.png",
+      name: "Sharon",
+      position: "BS in Computer Science",
+      comment: "Great Work",
+      description: `Participating in the University CTF 2023 was an incredible experience. The challenges were well-crafted, pushing us to think critically and apply our skills in real-time scenarios. The event was organized flawlessly, with excellent support and resources provided throughout. It truly showcased the university's commitment to fostering a strong cybersecurity community`,
+    },
+    {
+      id: 3,
+      imageSrc: "/assets/img/testimonials/1.png",
+      name: "aj",
+      position: "BS in Computer Science",
+      comment: "Great Work",
+      description: `Participating in the University CTF 2023 was an incredible experience. The challenges were well-crafted, pushing us to think critically and apply our skills in real-time scenarios. The event was organized flawlessly, with excellent support and resources provided throughout. It truly showcased the university's commitment to fostering a strong cybersecurity community`,
+    },
+    {
+      id: 4,
+      imageSrc: "/assets/img/testimonials/1.png",
+      name: "mAC",
+      position: "BS in Computer Science",
+      comment: "Great Work",
+      description: `Participating in the University CTF 2023 was an incredible experience. The challenges were well-crafted, pushing us to think critically and apply our skills in real-time scenarios. The event was organized flawlessly, with excellent support and resources provided throughout. It truly showcased the university's commitment to fostering a strong cybersecurity community`,
+    },
+    {
+      id: 5,
+      imageSrc: "/assets/img/testimonials/1.png",
+      name: "JOLO",
+      position: "BS in Computer Science",
+      comment: "Great Work",
+      description: `Participating in the University CTF 2023 was an incredible experience. The challenges were well-crafted, pushing us to think critically and apply our skills in real-time scenarios. The event was organized flawlessly, with excellent support and resources provided throughout. It truly showcased the university's commitment to fostering a strong cybersecurity community`,
+    },
+    {
+      id: 6,
+      imageSrc: "/assets/img/testimonials/1.png",
+      name: "HAMSEC",
+      position: "BS in Computer Science",
+      comment: "Great Work",
+      description: `Participating in the University CTF 2023 was an incredible experience. The challenges were well-crafted, pushing us to think critically and apply our skills in real-time scenarios. The event was organized flawlessly, with excellent support and resources provided throughout. It truly showcased the university's commitment to fostering a strong cybersecurity community`,
+    },
+    {
+      id: 7,
+      imageSrc: "/assets/img/testimonials/1.png",
+      name: "VINA",
+      position: "BS in Computer Science",
+      comment: "Great Work",
+      description: `Participating in the University CTF 2023 was an incredible experience. The challenges were well-crafted, pushing us to think critically and apply our skills in real-time scenarios. The event was organized flawlessly, with excellent support and resources provided throughout. It truly showcased the university's commitment to fostering a strong cybersecurity community`,
+    },
+    {
+      id: 8,
+      imageSrc: "/assets/img/testimonials/1.png",
+      name: "DREW",
+      position: "BS in Computer Science",
+      comment: "Great Work",
+      description: `Participating in the University CTF 2023 was an incredible experience. The challenges were well-crafted, pushing us to think critically and apply our skills in real-time scenarios. The event was organized flawlessly, with excellent support and resources provided throughout. It truly showcased the university's commitment to fostering a strong cybersecurity community`,
+    },
+  
+  ];
+  
+  
+  
+  
