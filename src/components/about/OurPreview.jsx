dcc7cdc2a3dae2cc179b37d5559cf97c5ccac35a
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-17 01:35:25 +0800

    Create OurPreview.jsx

---
+++ src/components/about/OurPreview.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import React from "react";
+
+export default function OurPreview() {
+  return (
+    <section className="cta -type-1 layout-pt-lg layout-pb-lg">
+      <div data-parallax="0.6" className="cta__bg">
+        <div
+          data-parallax-target
+          className="bg-image js-lazy"
+          style={{ backgroundImage: "url(/assets/img/about/whoweare/bg.png)" }}
+        ></div>
+      </div>
+    </section>
+  );
+}
