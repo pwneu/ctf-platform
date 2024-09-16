558c2cbcef3d4259fdf0ed09a21310ba3253d139
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-17 01:24:05 +0800

    Create herostory

---
+++ src/data/herostory.js
@@ -0,0 +1,41 @@
+export const slidesData = [
+    {
+        id: 1,
+        bgImage: "/assets/img/about/herostory/mainslider/bg.png",
+    },
+    {
+        id: 2,
+        bgImage: "/assets/img/about/herostory/mainslider/bg.png",
+    },
+    {
+        id: 3,
+        bgImage: "/assets/img/about/herostory/mainslider/bg.png",
+    },
+];
+
+export const feature = [
+    {
+        id: 1,
+        imgSrc: "/assets/img/about/herostory/icons/1.svg",
+        title: "New Era University",
+        description: "Computer Studies",
+    },
+    {
+        id: 2,
+        imgSrc: "/assets/img/about/herostory/icons/2.svg",
+        title: "New Era University",
+        description: "Information Technology",
+    },
+    {
+        id: 3,
+        imgSrc: "/assets/img/about/herostory/icons/3.svg",
+        title: "New Era University",
+        description: "Information System",
+    },
+    {
+        id: 3,
+        imgSrc: "/assets/img/about/herostory/icons/3.svg",
+        title: "New Era University",
+        description: "Information System",
+    },
+];
