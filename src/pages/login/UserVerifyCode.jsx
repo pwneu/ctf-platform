fafe8ed8175e92c7e8adf5be180e5a3fc8f6bb11
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-20 03:41:11 +0800

    Create Verification logic

---
+++ src/pages/login/UserVerifyCode.jsx
@@ -0,0 +1,29 @@
+
+import Preloader from "@/components/common/Preloader"; 
+import HeaderAuth from "@/components/layout/headers/HeaderAuth";
+import AuthImageMove from "@/components/others/AuthImageMove";
+import VerifyCode from "@/pages/login/VerifyCode"; 
+import React from "react";
+import MetaComponent from "@/components/common/MetaComponent"; 
+
+const metadata = {
+  title: "Verify Code || PWNEU",
+  description: "Enter the verification code sent to your email to confirm your identity and proceed.",
+};
+
+export default function VerifyCodePage() {
+  return (
+    <div className="main-content  ">
+      <MetaComponent meta={metadata} />
+      <Preloader />
+
+      <HeaderAuth />
+      <div className="content-wrapper js-content-wrapper overflow-hidden">
+        <section className="form-page js-mouse-move-container">
+          <AuthImageMove />
+          <VerifyCode />
+        </section>
+      </div>
+    </div>
+  );
+}
