a997938e3f0984de63c31dc0b0a02699540063a1
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-20 03:53:23 +0800

    Update  App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,28 @@ import "aos/dist/aos.css";
 import { useEffect } from "react";
 
 import IndexWeb from "./pages/home/indexweb";
-import LoginPage from "./pages/login";
+
+import LoginPage from "./pages/login/Index";
+import ForgotPasswordPage from "./pages/login/UserForgotPassword";
+import VerifyCodePage from "./pages/login/UserVerifyCode";
+import SetPasswordPage from "./pages/login/UserSetPassword";
+import PasswordCompletedPage from "./pages/login/UserPasswordCompleted";
+
 import SignupPage from "./pages/signup/Index";
 import AccountCreatedPage from "./pages/signup/UserAccountCreated";
-import AccountHasVerified from "@/pages/signup/UserGetStarted";
+import AccountHasVerifiedPage from "@/pages/signup/UserGetStarted";
 
 import ContactPage from "./pages/contacts/contact";
 import PrivacyPolicyPage from "./pages/ownership/privacy-policy";
 import TermsandConditionsPage from "./pages/ownership/terms-and-conditions";
+
 import UniversityPage from "./pages/campuses/campuses";
-import HelpCenterPage from "./pages/others/help-center";
 import ListofChallenges from "./pages/challengesList/list-of-challenges";
+
 import OurStoryPage from "./pages/about/our-story";
 import WhoWeArePage from "./pages/about/who-we-are";
 import MissionVisionPage from "./pages/about/mission-vision";
-
+import HelpCenterPage from "./pages/others/help-center";
 
 import DiscussionPage from "./pages/community/discussion-forum";
 // import AchievementsdetailsPage from "./pages/achivements/achievements";
@@ -47,12 +54,17 @@ function App() {
             {/* Homepage */}
             <Route index element={<IndexWeb />} />
 
-            {/* Users */}
+            {/* User Login */}
             <Route path="login" element={<LoginPage />} />
+            <Route path="forgot-password" element={<ForgotPasswordPage />} />
+            <Route path="verify-code" element={<VerifyCodePage />} />
+            <Route path="set-new-password" element={<SetPasswordPage />} />
+            <Route path="password-completed" element={<PasswordCompletedPage />} />
+          
+            {/* User Registration */}
             <Route path="signup" element={<SignupPage />} />
             <Route path="user-account-created" element={<AccountCreatedPage />} />
-            <Route path="account-verified" element={<AccountHasVerified  />} />
-        
+            <Route path="account-verified" element={<AccountHasVerifiedPage  />} />
             <Route path="campuses" element={<UniversityPage />} />
 
             {/* About Us*/}
@@ -65,7 +77,6 @@ function App() {
             <Route path="help-center" element={<HelpCenterPage />} />
             <Route path="privacy-policy" element={<PrivacyPolicyPage />} />
             <Route path="terms-and-conditions" element={<TermsandConditionsPage />} />
-
 
             {/* Compete */}
             <Route path="list-of-challenges" element={<ListofChallenges />} />
