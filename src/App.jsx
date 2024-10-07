b25e7cfd2c0cd652459b9611ec5fe1ca26f8cf5c
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-10-08 01:22:07 +0800

    Organized import statements for better readability and maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,43 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./styles/index.scss";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "react-toastify/dist/ReactToastify.css";
 import "react-calendar/dist/Calendar.css";
 import { Routes, Route } from "react-router-dom";
-import AOS from "aos";
-import "aos/dist/aos.css";
 import { useEffect } from "react";
-
-import IndexWeb from "./pages/home/indexweb";
-
-import LoginPage from "./pages/login";
-import ForgotPasswordPage from "./pages/login/UserForgotPassword";
-import VerifyCodePage from "./pages/login/UserVerifyCode";
-import SetPasswordPage from "./pages/login/UserSetPassword";
-import PasswordCompletedPage from "./pages/login/UserPasswordCompleted";
-
-import SignupPage from "./pages/signup";
-import AccountCreatedPage from "./pages/signup/UserAccountCreated";
-import AccountHasVerifiedPage from "@/pages/signup/UserGetStarted";
-
-import ContactPage from "./pages/contacts/contact";
-import PrivacyPolicyPage from "./pages/ownership/privacy-policy";
-import TermsandConditionsPage from "./pages/ownership/terms-and-conditions";
-
-import UniversityPage from "./pages/campuses/campuses";
-import ListofChallenges from "./pages/challengesList/list-of-challenges";
-import ChallengeDetails from "./pages/challengeDetails/challenge-details";
-
-
-import OurStoryPage from "./pages/about/our-story";
-import WhoWeArePage from "./pages/about/who-we-are";
-import MissionVisionPage from "./pages/about/mission-vision";
-import HelpCenterPage from "./pages/others/help-center";
-
-import DiscussionPage from "./pages/community/discussion-forum";
 import { ToastContainer } from "react-toastify";
+
+// Authentication-related pages
+import LoginPage from "./features/authentication/login";
+import ForgotPasswordPage from "./features/authentication/login/UserForgotPassword";
+import VerifyCodePage from "./features/authentication/login/UserVerifyCode";
+import SetPasswordPage from "./features/authentication/login/UserSetPassword";
+import PasswordCompletedPage from "./features/authentication/login/UserPasswordCompleted";
+import VerifyEmailPage from "./features/authentication/signup/VerifyEmail";
+import SignupPage from "./features/authentication/signup";
+import AccountCreatedPage from "./features/authentication/signup/UserAccountCreated";
+import AccountHasVerifiedPage from "@/features/authentication/signup/UserGetStarted";
+
+// Main application pages
+import HomePage from "./pages/HomePage.jsx";
+import ContactPage from "./pages/ContactPage";
+import PrivacyPolicyPage from "./pages/PrivacyPolicyPage";
+import TermsandConditionsPage from "./pages/TermsAndConditionsPage";
+import UniversityPage from "./pages/CampusesPage";
+import ListofChallenges from "./pages/ChallengeListPage";
+import ChallengeDetails from "./pages/ChallengesDetailsPage";
+
+// Informational pages
+import OurStoryPage from "./pages/OurStoryPage";
+import WhoWeArePage from "./pages/WhoWeArePage";
+import MissionVisionPage from "./pages/MissionVisionsPage";
+import HelpCenterPage from "./pages/HelpCenterPage";
+import DiscussionPage from "./pages/DiscussionForumsPage";
+
 // import AchievementsdetailsPage from "./pages/achivements/achievements";
 // import AchievementsListPage from "./pages/achivements/achievements-list";
-
-import "react-toastify/dist/ReactToastify.css"; // Make sure to import CSS
-import VerifyEmailPage from "./pages/signup/VerifyEmail";
 
 function App() {
   useEffect(() => {
@@ -56,26 +54,25 @@ function App() {
       <Routes>
         <Route path="/">
           {/* Homepage */}
-          <Route index element={<IndexWeb />} />
+          <Route index element={<HomePage />} />
 
-          {/* User Login */}
+          {/* User Authentication */}
           <Route path="login" element={<LoginPage />} />
           <Route path="forgot-password" element={<ForgotPasswordPage />} />
           <Route path="verify-code" element={<VerifyCodePage />} />
           <Route path="set-new-password" element={<SetPasswordPage />} />
-          <Route
-            path="password-completed"
-            element={<PasswordCompletedPage />}
-          />
+          <Route path="password-completed" element={<PasswordCompletedPage />}/>
           <Route path="verify-email" element={<VerifyEmailPage />} />
 
           {/* User Registration */}
           <Route path="signup" element={<SignupPage />} />
           <Route path="user-account-created" element={<AccountCreatedPage />} />
           <Route path="account-verified" element={<AccountHasVerifiedPage />} />
+
+          {/* Main Application Pages */}
           <Route path="campuses" element={<UniversityPage />} />
 
-          {/* About Us*/}
+          {/* About Us */}
           <Route path="our-story" element={<OurStoryPage />} />
           <Route path="who-we-are" element={<WhoWeArePage />} />
           <Route path="mission-vision" element={<MissionVisionPage />} />
@@ -84,12 +81,9 @@ function App() {
           <Route path="contact" element={<ContactPage />} />
           <Route path="help-center" element={<HelpCenterPage />} />
           <Route path="privacy-policy" element={<PrivacyPolicyPage />} />
-          <Route
-            path="terms-and-conditions"
-            element={<TermsandConditionsPage />}
-          />
+          <Route path="terms-and-conditions" element={<TermsandConditionsPage />} />
 
-          {/* Compete */}
+          {/* Challenges */}
           <Route path="list-of-challenges" element={<ListofChallenges />} />
           <Route path="challengeDetails/:id" element={<ChallengeDetails />} />
 
