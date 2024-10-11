f4f0d90851740a0be028e2908516a3c1be00b713
David King Roderos <dk.roderos@proton.me>
2024-10-11 19:11:47 +0800

    Implement reset password

--- src/pages/authentication/index.js
+++ src/pages/authentication/index.js
@@ -1,18 +1,14 @@
-import AccountVerifiedPage from "./AccountVerifiedPage";
 import ForgotPasswordPage from "./ForgotPasswordPage";
 import LoginPage from "./LoginPage";
-import PasswordCompletedPage from "./PasswordCompletedPage";
-import SetPasswordPage from "./SetPasswordPage";
+import ResetPasswordPage from "./ResetPasswordPage";
 import SignupPage from "./SignupPage";
 import VerifyCodePage from "./VerifyCodePage";
 import VerifyEmailPage from "./VerifyEmailPage";
 
 export {
-  AccountVerifiedPage,
   ForgotPasswordPage,
   LoginPage,
-  PasswordCompletedPage,
-  SetPasswordPage,
+  ResetPasswordPage,
   SignupPage,
   VerifyCodePage,
   VerifyEmailPage,
