6fba9b43580b3a87ee16c58079cce55ce132f02c
David King Roderos <dk.roderos@proton.me>
2024-10-16 02:56:09 +0800

    Implement challenges filtering

--- src/features/challenges/components/ChallengesPagination.jsx
+++ src/features/challenges/components/ChallengesPagination.jsx
@@ -1,18 +1,61 @@
-export default function ChallengesPagination({ page, setPage }) {
+export default function ChallengesPagination({
+  page,
+  setPage,
+  pageSize,
+  totalChallengesCount,
+}) {
+  const totalPages = Math.ceil(totalChallengesCount / pageSize);
+
+  const handlePrevious = () => {
+    if (page > 1) {
+      setPage((prev) => prev - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (page < totalPages) {
+      setPage((prev) => prev + 1);
+    }
+  };
+
+  const handlePageClick = (pageNumber) => {
+    setPage(pageNumber);
+  };
+
+  const renderPageNumbers = () => {
+    const pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(
+        <a
+          key={i}
+          onClick={() => handlePageClick(i)}
+          className={`cursor ${page === i ? "-count-is-active" : ""}`}
+        >
+          {i}
+        </a>
+      );
+    }
+    return pageNumbers;
+  };
+
   return (
     <div className="row justify-center pt-90 lg:pt-50">
       <div className="col-auto">
         <div className="pagination -buttons">
           <button
-            className="pagination__button -prev "
-            onClick={() => setPage(page - 1)}
+            className="pagination__button -prev"
+            onClick={handlePrevious}
+            disabled={page === 1} // Disable if on the first page
           >
             <i className="icon icon-chevron-left"></i>
           </button>
 
+          <div className="pagination__count">{renderPageNumbers()}</div>
+
           <button
-            onClick={() => setPage(page + 1)}
             className="pagination__button -next"
+            onClick={handleNext}
+            disabled={page >= totalPages} // Disable if on the last page
           >
             <i className="icon icon-chevron-right"></i>
           </button>
--- src/features/challenges/data/challengesFilterOptions.js
+++ src/features/challenges/data/challengesFilterOptions.js
@@ -1,5 +1,8 @@
 export const sortByOptions = ["Name", "Points", "Solves"];
-export const sortOrderOptions = ["Ascending", "Descending"];
+export const sortOrderOptions = [
+  { id: "asc", value: "Ascending" },
+  { id: "desc", value: "Descending" },
+];
 export const excludeSolveOptions = [
   { id: false, value: "No" },
   { id: true, value: "Yes" },
