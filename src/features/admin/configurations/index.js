d59b13ab2c46267650b9f682baa4da5750b2ff38
David King Roderos <dk.roderos@proton.me>
2024-10-21 11:42:54 +0800

    Separate configurations

---
+++ src/features/admin/configurations/index.js
@@ -0,0 +1,13 @@
+import CertificationIssuer from "./components/CertificationIssuer";
+import IsCertificationEnabled from "./components/IsCertificationEnabled";
+import IsTurnstileEnabled from "./components/IsTurnstileEnabled";
+import PublicLeaderboardCount from "./components/PublicLeaderboardCount";
+import SubmissionsAllowed from "./components/SubmissionsAllowed";
+
+export {
+  CertificationIssuer,
+  IsCertificationEnabled,
+  IsTurnstileEnabled,
+  PublicLeaderboardCount,
+  SubmissionsAllowed,
+};
