7518d19382eee3d61310e6f10af391fc82b2e468
David King Roderos <dk.roderos@proton.me>
2024-11-29 16:29:17 +0800

    Implement downloading leaderboards

--- src/pages/admin/LeaderboardsAdminPage.jsx
+++ src/pages/admin/LeaderboardsAdminPage.jsx
@@ -6,6 +6,8 @@ import useAuth from "@/hooks/useAuth";
 import HeaderAdmin from "@/layout/headers/HeaderAdmin";
 import { useNavigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt, faDownload } from "@fortawesome/free-solid-svg-icons";
 
 export default function LeaderboardsAdminPage() {
   const navigate = useNavigate();
@@ -17,6 +19,9 @@ export default function LeaderboardsAdminPage() {
   const [isClearingLeaderboardsCache, setIsClearingLeaderboardsCache] =
     useState(false);
 
+  const [isDownloadingLeaderboards, setIsDownloadingLeaderboards] =
+    useState(false);
+
   const getLeaderboards = async () => {
     try {
       const response = await api.get("/play/leaderboards");
@@ -24,6 +29,40 @@ export default function LeaderboardsAdminPage() {
     } catch {
       toast.error("Something went wrong getting leaderboards");
       setLeaderboards([]);
+    }
+  };
+
+  const downloadLeaderboards = async () => {
+    try {
+      setIsDownloadingLeaderboards(true);
+
+      const response = await api.get("/play/leaderboards/download", {
+        responseType: "blob",
+      });
+
+      const fileURL = window.URL.createObjectURL(new Blob([response.data]));
+
+      const link = document.createElement("a");
+      link.href = fileURL;
+      link.download = "PWNEU leaderboards.pdf";
+
+      link.click();
+
+      window.URL.revokeObjectURL(fileURL);
+    } catch (error) {
+      const status = error?.response?.status;
+
+      if (status === 401) {
+        navigate("/login");
+      } else if (status === 429) {
+        toast.warn("Slow down on downloading leaderboards!");
+      } else {
+        toast.error(
+          "Something went wrong downloading leaderboards. Please try again later"
+        );
+      }
+    } finally {
+      setIsDownloadingLeaderboards(false);
     }
   };
 
@@ -45,7 +84,7 @@ export default function LeaderboardsAdminPage() {
         toast.warn("Slow down!");
       } else {
         toast.error(
-          "Something went wrong getting clearing leaderboards cache. Please try again later"
+          "Something went wrong clearing leaderboards cache. Please try again later"
         );
       }
     } finally {
@@ -83,15 +122,31 @@ export default function LeaderboardsAdminPage() {
           leaderboards.userRanks && leaderboards.userRanks.length > 0 ? (
             <>
               <LeaderboardGraph topUsersGraph={leaderboards.topUsersGraph} />
+
               <Button
                 onClick={clearLeaderboardsCache}
-                className="mb-3"
+                className="mb-3 me-2"
+                variant="danger"
                 disabled={!isAdmin || isClearingLeaderboardsCache}
               >
+                <FontAwesomeIcon icon={faTrashAlt} className="me-2" />
                 {isClearingLeaderboardsCache
                   ? "Clearing..."
                   : "Clear Leaderboards Cache"}
               </Button>
+
+              <Button
+                onClick={downloadLeaderboards}
+                className="mb-3"
+                variant="success"
+                disabled={isDownloadingLeaderboards}
+              >
+                <FontAwesomeIcon icon={faDownload} className="me-2" />
+                {isDownloadingLeaderboards
+                  ? "Downloading..."
+                  : "Download Leaderboards"}
+              </Button>
+
               <UserRanksAdmin
                 requesterRank={leaderboards.requesterRank}
                 userRanks={leaderboards.userRanks}
