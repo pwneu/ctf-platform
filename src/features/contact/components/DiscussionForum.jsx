be3ca5fe73fc87d66b910e822f513b4cc8bba3f5
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-12-04 17:39:57 +0800

    Add new Community layout

--- src/features/contact/components/DiscussionForum.jsx
+++ src/features/contact/components/DiscussionForum.jsx
@@ -3,36 +3,51 @@ import { Link } from "react-router-dom";
 export default function DiscussionForum() {
   return (
     <>
-      <section className="layout-pt-md layout-pb-lg">
-        <div className="container">
-          <div className="row y-gap-60 justify-between items-center">
-            <div className="col-lg-5">
-              <h4 className="text-45 lh-16">
-                  Join Our Community
-              </h4>
-              <p className="text-dark-1 mt-30 text-19">
-                Connect with like-minded individuals in our all-in-one community! Stay updated with learner stories, upcoming events, 
-                and join our active Discord and forums. PWNEU is your go-to place for everything you need in the world of cybersecurity education.
-              </p>
-            
-              <div className="d-inline-block">
-                <Link
-                  to="/signup"
-                  className="button -md -purple-1 text-white mt-50"
-                >
-                  Join Now
-                </Link>
-              </div>
-            </div>
-            <div className="col-lg-6 pr-50 sm:pr-15">
-              <div className="composition -type-8">
-               <img
-                    className="w-1/1"
-                    src="/assets/img/community/Community_Discord.svg"
-                    alt="image"
-                  />
-              </div>
-            </div>
+      <section
+        className="py-16 bg-center bg-no-repeat"
+        style={{
+          backgroundImage: "url('assets/img/community/Community-bg.svg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "100vh",
+        }}
+      >
+        <div className="container mx-auto flex flex-col justify-center items-center h-full ">
+          <div className="text-center">
+            <img
+              src="assets/img/community/DiscordCommunity.svg"
+              alt="Community Icon"
+              style={{
+                marginTop: "150px", 
+                width: "90%",
+                maxWidth: "380px",
+              }}
+            />
+          </div>
+
+          <div className="text-white mt-6 px-4 text-center max-w-2xl">
+            <h4 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold text-white leading-tight">
+              Connect with Our Cybersecurity Community
+            </h4>
+            <p className="mt-4 leading-relaxed content-wrapper js-content-wrapper overflow-hidden">
+              Connect with like-minded individuals in our all-in-one community!
+              Stay updated with learner stories, upcoming events, and join our
+              active Discord and forums. <br></br>PWNEU is your go-to place for
+              everything you need in the world of cybersecurity education.
+            </p>
+          </div>
+          <div
+            className="flex justify-center items-center mt-30 mx-auto"
+            style={{
+              width: "300px",
+            }}
+          >
+            <Link
+              to="/signup"
+              className="button -sm -white text-dark-1 font-semibold rounded-lg shadow hover:bg-gray-200"
+            >
+              Join Now
+            </Link>
           </div>
         </div>
       </section>
