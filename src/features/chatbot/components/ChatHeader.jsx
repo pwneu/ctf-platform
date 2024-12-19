e78e786c4eeed31259ca4873c75af532dbb26fd8
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-12-20 02:10:59 +0800

    Add animation in chat interface

--- src/features/chatbot/components/ChatHeader.jsx
+++ src/features/chatbot/components/ChatHeader.jsx
@@ -1,14 +1,24 @@
 const ChatHeader = () => (
   <section className="layout-pt-lg  bg-dark-3">
     <div className="container">
-      <div
-        className="row y-gap-30 justify-between items-center"
-      >
+      <div className="row y-gap-30 justify-between items-center">
         <div className="col-xl-7 col-lg-4">
-          <div className="sectionTitle -light"
-          >
-            <h2 className="sectionTitle__title mt-2 "> {"Dash AI Chatbot (Experimental v0.0.1) "} </h2>
-            <p className="sectionTitle__text mt-30 ">
+          <div className="sectionTitle -light">
+            <h2
+              className="sectionTitle__title mt-2 "
+              data-aos="fade-down"
+              data-aos-offset="80"
+              data-aos-duration={900}
+            >
+              {" "}
+              {"Dash AI Chatbot (Experimental v0.0.1) "}{" "}
+            </h2>
+            <p
+              className="sectionTitle__text mt-30 "
+              data-aos="fade-up"
+              data-aos-offset="80"
+              data-aos-duration={900}
+            >
               Learn to use Dash AI, the advanced chatbot designed for CTF
               challenges. Get real-time hints, guidance, and interactive
               problem-solving to enhance your cybersecurity skills and tackle
@@ -16,7 +26,12 @@ const ChatHeader = () => (
             </p>
           </div>
         </div>
-        <div className="col-lg-4 lg:order-4">
+        <div
+          className="col-lg-4 lg:order-4"
+          data-aos="fade-up"
+          data-aos-offset="80"
+          data-aos-duration={900}
+        >
           <img
             className="w-1/1"
             src="assets/img/chatbot/dashai-icons.png"
