d547ae5559881d976c7d89acb6f172e9369c2dd1
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-12-24 03:44:28 +0800

    Add hints activity

---
+++ src/features/user-profile/data/hintusage.js
@@ -0,0 +1,115 @@
+export const hintusage = [
+  {
+    id: 1,
+    imgSrc: "/assets/img/misc/2.png",
+    hintId: "728a8ae3-f732-4c2b-bc69-b0e049f7eefe",
+    hintCategory: "Web Exploitation",
+    deductionPoints: "150",
+    challengeId: "ee798e2e-cdae-4043-9c61-1d70cf94144a",
+    challengeName: "Future life.",
+    UsedAt: "12/18/2024, 2:43:27 PM",
+
+  },
+  {
+    id: 2,
+    imgSrc: "/assets/img/misc/2.png",
+    hintId: "3a517ab0-ebeb-458b-9e3b-d6ed8830e172",
+    hintCategory: "General Skills",
+    deductionPoints: "150",
+    challengeId: "113cc13d-d8b0-4501-a95c-0ac09b2a42d1",
+    challengeName: "Free world.",
+    UsedAt: "12/18/2024, 2:43:29 PM",
+
+  },
+  {
+    id: 3,
+    imgSrc: "/assets/img/misc/2.png",
+    hintId: "06bc3b08-0134-4745-b608-f7d3c0e6a9b5",
+    hintCategory: "Cyptogrampy",
+    deductionPoints: "100",
+    challengeId: "Programming",
+    challengeName: "Here hand doctor.",
+    UsedAt: "12/18/2024, 2:43:29 PM",
+  },
+  {
+    id: 4,
+    imgSrc: "/assets/img/misc/2.png",
+    hintId: "b20ccb7d-d97c-406e-b35d-43cbebca8c02",
+    hintCategory: "Web exploitation",
+    deductionPoints: "150",
+    challengeId: "728a8ae3-f732-4c2b-bc69-b0e049f7eefe",
+    challengeName: "Bad foreign rate",
+    UsedAt: "12/18/2024, 2:43:28 PM",
+
+  },
+  {
+    id: 5,
+    imgSrc: "/assets/img/misc/2.png",
+    hintId: "728a8ae3-f732-4c2b-bc69-b0e049f7eefe",
+    hintCategory: "Forensics",
+    deductionPoints: "300",
+    challengeId: "b20ccb7d-d97c-406e-b35d-43cbebca8c02",
+    challengeName: "Wear threat.",
+    UsedAt: "12/18/2024, 2:43:27 PM",
+
+  },
+
+  {
+    id: 1,
+    imgSrc: "/assets/img/misc/2.png",
+    hintId: "728a8ae3-f732-4c2b-bc69-b0e049f7eefe",
+    hintCategory: "Web Exploitation",
+    deductionPoints: "150",
+    challengeId: "ee798e2e-cdae-4043-9c61-1d70cf94144a",
+    challengeName: "Future life.",
+    UsedAt: "12/18/2024, 2:43:27 PM",
+
+  },
+  {
+    id: 2,
+    imgSrc: "/assets/img/misc/2.png",
+    hintId: "3a517ab0-ebeb-458b-9e3b-d6ed8830e172",
+    hintCategory: "General Skills",
+    deductionPoints: "150",
+    challengeId: "113cc13d-d8b0-4501-a95c-0ac09b2a42d1",
+    challengeName: "Free world.",
+    UsedAt: "12/18/2024, 2:43:29 PM",
+
+  },
+  {
+    id: 3,
+    imgSrc: "/assets/img/misc/2.png",
+    hintId: "06bc3b08-0134-4745-b608-f7d3c0e6a9b5",
+    hintCategory: "Cyptogrampy",
+    deductionPoints: "100",
+    challengeId: "Programming",
+    challengeName: "Here hand doctor.",
+    UsedAt: "12/18/2024, 2:43:29 PM",
+  },
+  {
+    id: 4,
+    imgSrc: "/assets/img/misc/2.png",
+    hintId: "b20ccb7d-d97c-406e-b35d-43cbebca8c02",
+    hintCategory: "Web exploitation",
+    deductionPoints: "150",
+    challengeId: "728a8ae3-f732-4c2b-bc69-b0e049f7eefe",
+    challengeName: "Bad foreign rate",
+    UsedAt: "12/18/2024, 2:43:28 PM",
+
+  },
+  {
+    id: 5,
+    imgSrc: "fa fa-trophy",
+    hintId: "728a8ae3-f732-4c2b-bc69-b0e049f7eefe",
+    hintCategory: "Forensics",
+    deductionPoints: "300",
+    challengeId: "b20ccb7d-d97c-406e-b35d-43cbebca8c02",
+    challengeName: "Wear threat.",
+    UsedAt: "12/18/2024, 2:43:27 PM",
+
+  },
+
+
+
+
+];
