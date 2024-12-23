540925ebf69c5cd0562ae38015a301e328197bce
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-12-24 03:42:18 +0800

    Add user's overall activity

---
+++ src/features/user-profile/data/sidebarprofile.js
@@ -0,0 +1,50 @@
+export const sidebarprofile = [
+  {
+    id: 1,
+    href: "/play",
+    iconClass: "fa fa-gamepad",
+    text: "CTF Challenges",
+  },
+  {
+    id: 2,
+    href: "/dashboard",
+    iconClass: "fa fa-pie-chart",
+    text: "Dashboard",
+  },
+  {
+    id: 3,
+    href: "/user-summary",
+    text: "Summary",
+    iconClass: "icon-bar-chart",
+  },
+  {
+    id: 4,
+    href: "/solve-overview",
+    text: "Solve Overview",
+    iconClass: "icon-search",
+  },
+  {
+    id: 5,
+    href: "/hint-usage",
+    text: "Hint Usage",
+    iconClass: "fa fa-lightbulb",
+  },
+  {
+    id: 6,
+    href: "/certify",
+    text: "Certification",
+    iconClass: "fa fa-certificate",
+  },
+  {
+    id: 7,
+    href: "/settings",
+    iconClass: "icon-tools",
+    text: "Settings",
+  },
+  {
+    id: 8,
+    href: "/",
+    iconClass: "icon-power",
+    text: "Logout",
+  },
+];
