0a1e96086d0fbe577b7ed5a646e8fb2e41f6eff8
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-12-24 03:26:02 +0800

    Update layout components for user profiles

---
+++ src/layout/headers/HeaderProfile.jsx
@@ -0,0 +1,74 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+export default function HeaderProfile() {
+  useEffect(() => {
+    if (window.innerWidth < 990) {
+      document
+        .getElementById("dashboardOpenClose")
+        .classList.add("-is-sidebar-hidden");
+    }
+    const handleResize = () => {
+      if (window.innerWidth < 990) {
+        document
+          .getElementById("dashboardOpenClose")
+          .classList.add("-is-sidebar-hidden");
+      }
+    };
+
+    // Add event listener to window resize event
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener when component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return (
+    <>
+      <header className="header -dashboard -dark-bg-dark-1 js-header">
+        <div className="header__container py-20 px-30">
+          <div className="row justify-between items-center">
+            <div className="col-auto">
+              <div className="d-flex items-center">
+                <div className="header__explore text-dark-1">
+                  <button
+                    onClick={() => {
+                      document
+                        .getElementById("dashboardOpenClose")
+                        .classList.toggle("-is-sidebar-hidden");
+                    }}
+                    className="d-flex items-center js-dashboard-home-9-sidebar-toggle"
+                  >
+                    <i className="icon -dark-text-white icon-explore"></i>
+                  </button>
+                </div>
+
+                <div className="header__logo ml-30 md:ml-20">
+                  <Link data-barba to="/">
+                    <img
+                      className="-dark-d-none"
+                      src="/assets/img/general/PWNEU_UserLogoHeader.png"
+                      alt="logo"
+                      style={{ width: "90px", height: "auto" }}
+                    />
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-auto">
+              <div className="d-flex items-center">
+                <div className="relative d-flex items-center ml-10">
+                  <a href="#" data-el-toggle=".js-profile-toggle text-dark-1">
+                    Username here
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
