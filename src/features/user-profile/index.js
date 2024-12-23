4446ba623cf0bddfb192c095667caf22fddcf3fb
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-12-24 03:40:41 +0800

    Add profile dashboard

---
+++ src/features/user-profile/index.js
@@ -0,0 +1,30 @@
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./components/Dashboard";
+import Charts from "./components/Charts";
+import PieCharts from "./components/PieCharts";
+
+import UserReport from "./components/UserReport";
+import SolveOverview from "./components/SolveOverview";
+import HintUsage from "./components/HintUsage";
+import Certificate from "./components/Certificate";
+
+import Settings from "./components/Settings";
+import Password from "./components/Password";
+import EditProfile from "./components/EditProfile";
+
+import FooterProfile from "@/layout/footers/FooterProfile";
+
+export {
+  Sidebar,
+  Dashboard,
+  Charts,
+  PieCharts,
+  UserReport,
+  SolveOverview,
+  HintUsage,
+  Certificate,
+  Settings,
+  Password,
+  EditProfile,
+  FooterProfile
+};
---
+++ src/pages/user-profile/index.js
@@ -0,0 +1,15 @@
+import UserDashboardPage from "./UserDashboardPage";
+import UserSummaryPage from "./UserSummaryPage";
+import UserSolveOverviewPage from "./UserSolveOverviewPage";
+import UserHintUsagePage from "./UserHintUsagePage";
+import UserCertifyPage from "./UserCertifyPage";
+import UserSettingsPage from "./UserSettingsPage"
+
+export {
+  UserDashboardPage,
+  UserSummaryPage,
+  UserSolveOverviewPage,
+  UserHintUsagePage,
+  UserCertifyPage,
+  UserSettingsPage,
+};
