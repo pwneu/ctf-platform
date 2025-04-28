ce32b514fec8d9234ad3bd230cf126d246c35de6
David King Roderos <dk.roderos@gmail.com>
2025-04-28 18:34:26 +0800

    Add links on who we are page

--- src/data/teams.js
+++ src/data/teams.js
@@ -1,14 +1,14 @@
-
 export const developers = [
-    {
-      id: 1,
-      title: "David King  Roderos",
-      profession: "Software Engineer",
-    },
-    {
-      id: 2,
-      title: "Sharon Grace Hangaan",
-      profession: "Full-Stack Developer",
-    },
-    
-  ];
+  {
+    id: 1,
+    title: "David King Roderos",
+    profession: "Software Engineer",
+    url: "https://github.com/dkroderos",
+  },
+  {
+    id: 2,
+    title: "Sharon Grace Hangaan",
+    profession: "Full-Stack Developer",
+    url: "https://github.com/sghangaan",
+  },
+];
--- src/features/about/components/DevFounder.jsx
+++ src/features/about/components/DevFounder.jsx
@@ -56,10 +56,12 @@ export default function DevFounder() {
         </div>
 
         <div className="row y-gap-30 pt-60 lg:pt-50 justify-content-center">
-          {developers.map((elm, i) => (
+          {developers.map((elm) => (
             <div
-              key={i}
-              className="col-xl-3 col-lg-2 col-md-6 linkCustomTwo justify-content-center "
+              key={elm.id}
+              className="col-xl-3 col-lg-2 col-md-6 linkCustomTwo justify-content-center"
+              onClick={() => window.open(elm.url, "_blank")}
+              style={{ cursor: "pointer" }}
             >
               <div
                 className="categoryCard -type-3"
@@ -68,7 +70,7 @@ export default function DevFounder() {
                 data-aos-duration={900}
               >
                 <div className="categoryCard__content">
-                  <h4 className="categoryCard__title text-11 fw-700 text-white ">
+                  <h4 className="categoryCard__title text-11 fw-700 text-white">
                     {elm.title}
                   </h4>
                   <div className="categoryCard__text text-white text-11 lh-1 mt-5">
@@ -86,10 +88,10 @@ export default function DevFounder() {
             data-aos-offset="80"
             data-aos-duration={900}
           >
-            We are developers and CTF (Capture The Flag) players,
-            dedicated to providing a dynamic and engaging learning platform for
-            students pursuing cybersecurity at New Era University. Our mission
-            is to encourage and empower students by offering them the tools,
+            We are developers and CTF (Capture The Flag) players, dedicated to
+            providing a dynamic and engaging learning platform for students
+            pursuing cybersecurity at New Era University. Our mission is to
+            encourage and empower students by offering them the tools,
             resources, and support they need to excel in the field of
             cybersecurity. We believe in hands-on learning through real-world
             challenges. That&apos;s why we focus on creating an environment
